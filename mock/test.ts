582b85e3a8d43a1fdbcfcddacd5395201f74a4df
Howard Yeh <howard@richwhole.net>
2022-11-03 16:28:28 +0800

    test api

--- mock/test.ts
+++ mock/test.ts
@@ -1,48 +1,41 @@
 import { mockConfig } from './types'
+import _ from 'lodash'
+let token = Date.now()
+
 const list: mockConfig[] = [
   {
-    url: '-/api/CountryLimit/CheckAsync',
-    method: 'post',
-    response: {
-      Guid: '90ec247c-b527-43d3-8440-be68146ce461',
-      Success: false,
-      Code: '9992',
-      Message: 'WebsiteMaintenance:103.27.180.3',
-      Data: {
-        IsAllow: 1,
-        CountryCode: null,
-        ProvinceID: 0,
-        CityID: 0,
-        CountryEName: null,
-        RegionName: null,
-        CityName: null,
-        IP: '192.168.31.1',
-        MaintenanceStartTime: '2022/08/02 09:00:00',
-        MaintenanceEndTime: '2022/08/30 17:52:00',
-      },
+    url: '/api/check',
+    response({ config, headers }) {
+      config.timeout = _.random(1, 5) * 1000
+      if (headers.token === token) {
+        return {
+          token: Date.now(),
+          message: 'ok',
+        }
+      } else {
+        config.status = 401
+        return {
+          message: 'fail',
+        }
+      }
     },
   },
   {
-    url: '-/api/login',
-    method: 'post',
+    url: '/api/get',
     timeout: 3000,
     status: 200,
-    response: args => {
-      // 响应内容
-      return +args.body.password === 123456
-        ? {
-            Code: 0,
-            Message: '登录成功',
-            Data: {
-              Token: '@word(50, 100)', // @word()是mockjs的语法
-              Refresh_token: '@word(50, 100)', // refresh_token是用来重新生成token的
-            },
-          }
-        : {
-            Code: 400,
-            Message: '密码错误，请输入123456',
-            Args: args,
-          }
+    response({ config, headers }) {
+      config.timeout = _.random(1, 5) * 1000
+      if (headers.token === token) {
+        return {
+          message: 'ok',
+        }
+      } else {
+        config.status = 401
+        return {
+          message: 'fail',
+        }
+      }
     },
   },
 ]
