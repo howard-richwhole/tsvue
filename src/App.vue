0541eaa17b3f05fe6034e464d86d74504bc96a93
Howard Yeh <howard@richwhole.net>
2022-11-03 11:06:14 +0800

    node

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { login } from '@/api/user'
+
+login({ password: 123456 })
+  .then(data => {
+    console.log('0', data)
+  })
+  .catch(data => {
+    console.log('1', data)
+  })
 </script>
 
 <template lang="pug">
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
 import './style.sass'
-import { mountVue } from '@/utils'
 import App from './App.vue'
-import { login } from '@/api/user'
-
-login({ password: 123456 })
-  .then(data => {
-    console.log('0', data)
-  })
-  .catch(data => {
-    console.log('1', data)
-  })
+import { mountVue } from '@/utils'
 
 mountVue({ component: App, el: '#app' })
