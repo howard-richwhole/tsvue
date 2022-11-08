1c9bb72a2fefee51442a83761239554cb868e04a
Howard Yeh <howard@richwhole.net>
2022-11-08 14:53:25 +0800

    test

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -13,32 +13,33 @@ const headers: Record<string, string> = {
 }
 
 export default function <resp>(opts: reqOpts): Promise<resp> {
-  useToken()
-  const abortCtrl = new AbortController()
+  const cb = () => {
+    const url = new URL(opts.url, base.origin + base.pathname)
+    const webStore = useWebStore()
+    if (webStore.token) {
+      headers.authorization = `Bearer ${webStore.token}`
+    }
+    const abortCtrl = new AbortController()
 
-  const fetchOpts: RequestInit = {
-    method: opts.method,
-    headers: _.assign(headers, opts.headers),
-    signal: abortCtrl.signal,
-  }
+    const fetchOpts: RequestInit = {
+      method: opts.method,
+      headers: _.assign(headers, opts.headers),
+      signal: abortCtrl.signal,
+    }
 
-  const url = new URL(opts.url, base.origin + base.pathname)
-  if (!fetchOpts.method || fetchOpts.method.toLowerCase() === 'get') {
-    _.each(opts.data as Record<string, string>, (val: string, key: string) => {
-      url.searchParams.set(key, JSON.stringify(val))
+    if (!fetchOpts.method || fetchOpts.method.toLowerCase() === 'get') {
+      _.each(
+        opts.data as Record<string, string>,
+        (val: string, key: string) => {
+          url.searchParams.set(key, JSON.stringify(val))
+        },
+      )
+    } else {
+      fetchOpts.body = JSON.stringify(opts.data)
+    }
+    return fetch(url.href, fetchOpts).then(data => {
+      return data.json()
     })
-  } else {
-    fetchOpts.body = JSON.stringify(opts.data)
   }
-
-  return fetch(url.href, fetchOpts).then(data => {
-    return data.json()
-  })
-}
-
-function useToken(): void {
-  const webStore = useWebStore()
-  if (webStore.token) {
-    headers.authorization = `Bearer ${webStore.token}`
-  }
+  return cb()
 }
